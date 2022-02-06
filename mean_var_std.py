3307d20069f637befe4f5bec37124708f2027261
Brandon J. Lacy <Z3R0@duck.com>
2022-02-06 09:24:10 -0500

    Initial Commit

---
+++ mean_var_std.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def calculate(list):
+    """Utilizes Numpy to output the mean, variance, standard deviation, max, min, and sum of a 9 element list."""
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+
+    array = np.array(list).reshape((3, 3))
+
+    calculations = {
+        'mean': [array.mean(axis=0).tolist(), array.mean(axis=1).tolist(), array.mean().tolist()],
+        'variance': [array.var(axis=0).tolist(), array.var(axis=1).tolist(), array.var().tolist()],
+        'standard deviation': [array.std(axis=0).tolist(), array.std(axis=1).tolist(), array.std().tolist()],
+        'max': [array.max(axis=0).tolist(), array.max(axis=1).tolist(), array.max().tolist()],
+        'min': [array.min(axis=0).tolist(), array.min(axis=1).tolist(), array.min().tolist()],
+        'sum': [array.sum(axis=0).tolist(), array.sum(axis=1).tolist(), array.sum().tolist()]
+    }
+
+    return calculations
